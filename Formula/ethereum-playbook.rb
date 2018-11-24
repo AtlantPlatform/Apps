9dc169ce906ccbfe96f912656cf27a0cbc5d0294
Maxim <mk@atlant.io>
2018-11-24 19:08:21 +0300

    Update ethereum-playbook to v1.0.2

--- Formula/ethereum-playbook.rb
+++ Formula/ethereum-playbook.rb
@@ -3,18 +3,12 @@ require "formula"
 class EthereumPlaybook < Formula
   desc "CLI tool that configures and deploys Ethereum DApp infrastructures using a static specification."
   homepage "https://github.com/AtlantPlatform/ethereum-playbook"
-  url "https://github.com/AtlantPlatform/ethereum-playbook/archive/v1.0.1.tar.gz"
-  sha256 "090f60a635548e898cebd1d28766534e25d88081213270834e2c5010d8adff67"
+  url "https://github.com/AtlantPlatform/ethereum-playbook/archive/v1.0.2.tar.gz"
+  sha256 "e2faa8c03013b0601be4ef8d3a38bed34409a42dd8c185cd98831bb4e32e3692"
 
   head "https://github.com/AtlantPlatform/ethereum-playbook.git"
 
   depends_on "go" => :build
-
-  bottle do
-    root_url "https://homebrew.bintray.com/bottles-apps"
-    cellar :any_skip_relocation
-    sha256 "4ccf9d7c1eeb0d6944791edb9e4e22496d9cde8528572708e47bdf1d4c16b773" => :high_sierra
-  end
 
   def install
     ENV["GOPATH"] = buildpath
